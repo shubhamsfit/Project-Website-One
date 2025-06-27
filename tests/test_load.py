aef9ac0e26feb497ef1346f314bffca7cd044f0b
shubhamsfit <shubhamsfit@users.noreply.github.com>
2025-06-27 07:23:36 +0000

    deploy: d1f0b80ffa65ddbf4d9b0ae70c92da5b93321b82

--- tests/test_load.py
+++
@@ -1,7 +0,0 @@
-from pages.load_page import LoadPage
-
-def test_load_check(driver):
-    load_page = LoadPage(driver)
-    load_page.load()
-    assert "Project-Website-One" in driver.current_url, "❌ URL is incorrect."
-    print("✅ URL verified successfully.")       
